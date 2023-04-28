713d6cc8f50f7c6aad2e3f63834301b90cc4f515
LucasGabrielAntonete <lucasantonete@gmail.com>
2023-04-28 16:56:06 -0300

    Criação de modelos,cores, ano, preco e marca

--- garagem/admin.py
+++ garagem/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
-from garagem.models import Categoria, Marca
+from garagem.models import Modelo, Marca, Cor, veiculo   
 
-admin.site.register(Categoria)
+admin.site.register(Modelo)
 admin.site.register(Marca)
+admin.site.register(Cor)
+admin.site.register(veiculo)
+
--- garagem/models.py
+++ garagem/models.py
@@ -1,12 +1,34 @@
 from django.db import models
 
-class Categoria(models.Model):
-    descricao = models.CharField(max_length=100)
-    
+class Modelo(models.Model):
+    Descricao = models.CharField(max_length=100) 
 
     def __str__(self):
         return self.nome
 
 class Marca(models.Model):
-    nome = models.CharField(max_length=50)
-    nacionalidade = models.CharField(max_length=50)
+    Marca = models.CharField(max_length=50)
+    Nacionalidade = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.Marca
+
+class Cor(models.Model):
+    Cores = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.Cores
+
+
+     
+
+
+class veiculo(models.Model):
+        Marca = models.ForeignKey(Marca, on_delete=models.PROTECT, related_name="Marcas")
+        Modelo = models.ForeignKey(Modelo, on_delete=models.PROTECT, related_name="Modelo")
+        Cor = models.ForeignKey(Cor,on_delete=models.PROTECT, related_name="Coress")
+        Ano = models.IntegerField(default=0, null=True, blank=True)
+        Preco = models.DecimalField(max_digits=10, decimal_places=2, default=0, null=True, blank=True )
+        
+        def __str__(self): 
+             return f"{self.Marca}, {self.Modelo}, {self.Ano}, {self.Cor}"
